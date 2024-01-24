f6edbfe6489801d13f8b94bcd658856f940592c1
tommy <littleworm.code@gmail.com>
2024-01-24 15:31:23 +0700

    update clerk middle ware

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  publicRoutes: ["/", "/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"],
   afterAuth(auth, req) {
     if (auth.userId && auth.isPublicRoute) {
       let path = "/select-org";
